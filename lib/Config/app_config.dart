9ca7e65bcd734b98f7683844999527fe9f0d20d8
jasperslowik <jasper.slowik@icloud.com>
2025-06-21 17:35:21 +0200

    Update dependencies in pubspec.lock and pubspec.yaml, change API base URL in app_config.dart, and remove test.pdf file.

--- lib/Config/app_config.dart
+++ lib/Config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   static const String apiBaseUrl =
-      'https://uni-chatbot-e2bc39ffc8de.herokuapp.com';
+      "http://localhost:8000";
   static const String firebaseFilesPath = 'files';
 }
